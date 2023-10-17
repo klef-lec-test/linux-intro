4f5b707e6c8f32f0b8e4edd936abf8e254e0c712
haru-0205 <rm23081b@osaka-pct.ac.jp>
2023-10-17 19:25:45 +0900

    build(deps): update 1 file and create 2 files

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ export default function Home() {
     <div>
       <p className='text-4xl'>2023年度1年4組 時間割共有サイト</p>
       <p className='text-2xl text-slate-300'>1年4組 後期時間割</p>
-      <table className='border-collapse'>
+      <table className='border-collapse border-solid'>
         <thead>
           <tr>
             <th colSpan={6}>2023年度 1年4組 後期時間割表</th>
@@ -19,6 +19,12 @@ export default function Home() {
             <th>金曜日</th>
           </tr>
         </thead>
+        <tbody>
+          <tr>
+            <td>1</td>
+            <td rowSpan={2}>基礎物理1</td>
+          </tr>
+        </tbody>
       </table>
     </div> 
   )
