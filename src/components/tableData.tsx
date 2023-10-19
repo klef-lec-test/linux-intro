5ede9e6374dd731bdea1d5992e474f40bb5e9d49
haru-0205 <rm23081b@osaka-pct.ac.jp>
2023-10-19 16:52:57 +0900

    update 2 files, create 2 files and delete 37 files

---
+++ src/components/tableData.tsx
@@ -0,0 +1,8 @@
+export const columns = [
+  { Header: "時間", accessor: "time" },
+  { Header: "月曜日" , accessor: "man"},
+  { Header: "火曜日" , accessor: "tue"},
+  { Header: "水曜日" , accessor: "wed"},
+  { Header: "木曜日" , accessor: "thr"},
+  { Header: "金曜日" , accessor: "fri"}
+]
---
+++ src/pages/kosensai.tsx
@@ -0,0 +1,50 @@
+import React, { useState } from "react";
+import Link from "next/link";
+import "../app/globals.css"
+
+const Kosensai = () => {
+  let [usr1, setUsr1] = useState(0);
+  function Usr1(pnt: number) {
+    console.log(pnt)
+    setUsr1(usr1 + pnt);
+    console.log("Success!")
+    console.log(usr1)
+  }
+  function resetUsr1() {
+    setUsr1(0);
+  }
+  const text1 = Kosensai.getInitialProps;
+  console.log(text1);
+    return(
+    <div>
+      <div>
+        <h2 className="text-2xl text-indigo-400">2023年度 第59回 大阪公立大学工業高等専門学校 高専祭 1年4組クラス展示</h2>
+        <h1 className="text-6xl  text-indigo-800">KS魂 得点計算/集計システム</h1>
+      </div>
+      <div>
+        <h3 className="text-xl text-indigo-600">ローリングコインタワー</h3>
+      </div>
+      <div>
+        <p className="text-sm bg-rose-200 text-slate-700">Player1</p>
+        <p className="text-2xl">{usr1}</p>
+        <button className="text-xl m-1 p-2 bg-lime-200" onClick={() => Usr1(1)}>+1</button>
+        <button className="text-xl m-1 p-2 bg-amber-200" onClick={() => Usr1(-1)}>-1</button>
+        <button className="text-xl m-1 p-2 bg-lime-300" onClick={() => Usr1(10)}>+10</button>
+        <button className="text-xl m-1 p-2 bg-amber-300" onClick={() => Usr1(-10)}>-10</button>
+        <button className="text-xl m-1 p-1 bg-yellow-400" onClick={() => resetUsr1()}>Reset</button>
+      </div>
+      <div>
+        <button className="bg-rose-200 m-2">送信</button>
+        {/* <button className="bg-rose-400 p-2">ランキングへ</button> */}
+      </div>
+    </div>
+  )
+}
+
+Kosensai.getInitialProps = async () => {
+  const res = await fetch('http://localhost:8080/');
+  const text = await res.text();
+  return {text};
+}
+
+export default Kosensai;
