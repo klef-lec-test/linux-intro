e94dbbbcae87a3f68e23deff81e897583c70fc8c
haru-0205 <rm23081b@osaka-pct.ac.jp>
2023-10-23 19:45:59 +0900

    update postCreateCommnd.sh

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -1,1 +1,5 @@
 git clone https://github.com/haru-0205/timetable-frontend-next
+git config --global user.email #your email address
+git config --global user.name #your name
+gh auth login --with-token #your GitHub Access Token
+git checkout develop
