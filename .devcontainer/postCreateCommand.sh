ff5b5a2539965409a7a9d2818b2466b5d35de88d
haru-0205 <rm23081b@osaka-pct.ac.jp>
2023-10-28 07:32:41 +0900

    update postCreateCommand.sh

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -4,3 +4,8 @@ pacman -S --noconfirm nano
 pacman -S --noconfirm neovim
 
 git config --system core.autocrlf input
+// git config --global user.email //your email
+// git config --global user.name //your name
+// gh auth login --with-token //your GitHub Personal Access Token
+echo "設定が完了しました"
+echo "gitの設定を行うため, "./setup.sh"を実行してください．
