6bca7db6ace1448063f24f301d4b7d0b54ccb65f
haru-0205 <rm23081b@osaka-pct.ac.jp>
2023-10-28 07:23:15 +0900

    update dockerfile, postCreateCommand.sh and setup.sh

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -1,11 +1,6 @@
-# git clone https://github.com/haru-0205/timetable-frontend-next
-echo "メールアドレスを入力してください"
-read email
-git config --global user.email $email
-echo "ユーザー名を入力してください"
-read name
-git config --global user.name $name
-gh auth login
-git fetch
-git pull
-git checkout develop
+yarn add next
+yarn add react
+pacman -S --noconfirm nano
+pacman -S --noconfirm neovim
+
+git config --system core.autocrlf input
