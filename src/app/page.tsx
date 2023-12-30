a34a8145c86093442bf8305b0ee85aad2db89b41
Haru-0205 <rm23081b@osaka-pct.ac.jp>
2023-12-30 23:44:55 +0900

    update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,10 +4,14 @@ import TimeTable from '@/components/timetable';
 import ExamTemplate from '@/components/exam_temprate';
 import Header from '@/components/header';
 import WinterHw from '@/components/winter_hw';
+import Head from 'next/head';
 
 export default function Home() {
   return (
     <div>
+      <Head>
+        <title>Dashboard - 2023_1-4</title>
+      </Head>
       <Header title={"Dashboard"}></Header>
       <TimeTable />
       <WinterHw />
