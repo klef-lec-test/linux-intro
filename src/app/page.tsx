6effbf586c1ee1433ff4a4abb5c02fcddf952a72
haru-0205 <rm23081b@osaka-pct.ac.jp>
2024-01-20 16:13:56 +0900

    Update layout and page components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,7 @@
-<<<<<<< HEAD
 "use client"
 import Image from 'next/image';
 import React from 'react';
 import TimeTable from '@/components/timetable';
-=======
-import Image from 'next/image'
-import TimeTable from '@/components/timetable'
->>>>>>> parent of 02260d9 (update layout.tsx, page.tsx and exam_temprate.tsx)
 import ExamTemplate from '@/components/exam_temprate';
 import Header from '@/components/header';
 import WinterHw from '@/components/winter_hw';
@@ -24,11 +19,7 @@ export default function Home() {
       <TimeTable />
       <WinterHw />
       <ExamTemplate />
-<<<<<<< HEAD
       <Info />
     </div>
-=======
-    </div> 
->>>>>>> parent of 02260d9 (update layout.tsx, page.tsx and exam_temprate.tsx)
   )
 }
