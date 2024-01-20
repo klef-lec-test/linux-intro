79a57330a4af87e1be56dbc43dbef5807914e916
haru-0205 <rm23081b@osaka-pct.ac.jp>
2024-01-20 20:10:20 +0900

    Add new files and update dependencies

--- src/components/infomations.tsx
+++ src/components/infomations.tsx
@@ -2,9 +2,14 @@ import { Paper } from "@mui/material"
 
 const INFOMATIONS = [
   {
+    title: "不具合報告(Issue)について",
+    date: "2024.01.20",
+    contents: "本ページの不具合につきましては、GitHubのIssueにて受け付けております。不具合の報告は、下記のリンクからお願いいたします。\n<a href=\"https://github.com/haru-0205/timetable-frontend-next/issues\">https://github.com/haru-0205/timetable-frontend-next/issues\</a>\nなお、詳しいやり方は<a href=\"https://timetable-frontend-next.vercel.app/help/how-to-raise-issue\">こちら</a>をご覧ください。"
+  },
+  {
     title: "本ページの不具合について",
     date: "2024.01.20",
-    contents: ""
+    contents: "本ページの「学年末試験」の欄において、「あとx日」の部分が更新されないという不具合が発生しておりました。ご利用の皆様には多大なるご迷惑をおかけしたことをお詫び申し上げます。\nなお、本不具合につきましては、現在、当方で解決に向けて作業をしております。完了次第こちらに掲載いたします。\nご不便をおかけしますが、ご理解、ご協力のほどよろしくお願いいたします。"
   },
   {
     title: "このページについて",
@@ -25,7 +30,7 @@ const Infomations = () => {
         <Paper>
           <h3 className="text-lg">{item.title}</h3>
           <p className="text-blue-900">{item.date}</p>
-          <p>{item.contents}</p>
+          <div className="whitespace-pre-wrap" dangerouslySetInnerHTML={{__html: item.contents}}></div>
         </Paper>
       )
     })
