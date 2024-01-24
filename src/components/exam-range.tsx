7f5eeee0c230e2fdc20f38734d22707628861340
haru-0205 <rm23081b@osaka-pct.ac.jp>
2024-01-24 16:36:50 +0900

    Refactor exam-range component to display homework without HTML formatting

--- src/components/exam-range.tsx
+++ src/components/exam-range.tsx
@@ -9,7 +9,7 @@ const ExamRange = () => {
         <Paper>
           <h3 className="text-lg">{item.subject}</h3>
           <p>範囲：<span className="whitespace-pre-wrap" dangerouslySetInnerHTML={{__html: item.range}}></span></p>
-          <p>課題：<span className="whitespace-pre-wrap" dangerouslySetInnerHTML={{__html: item.homework}}></span></p>
+          <p>課題 : {item.homework}</p>
         </Paper>
       )
     })
