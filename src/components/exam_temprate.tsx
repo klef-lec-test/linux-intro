a8e52b66a78d6e8f6ce885388fe9fc1aaf633de3
haru-0205 <rm23081b@osaka-pct.ac.jp>
2024-02-23 18:57:25 +0900

    update settings.json and exam_temprate.tsx

--- src/components/exam_temprate.tsx
+++ src/components/exam_temprate.tsx
@@ -9,7 +9,10 @@ const ExamTemplate: NextPage = () => {
   const exam_day: any = new Date("2024, 2, 8");
   let days: any = (exam_day - today) / 86400000;
   days = Math.ceil(days);
-  return(
+  if (days < 0) {
+    days = 0;
+  }
+  return (
     <div>
       <Paper>
         <h2 className="text-2xl">学年末試験<span className="text-xl">(あと{days}日)</span></h2>
